f24bad1c53439456f56d198f9848d9619795fa47
Voltxt.io <gossdhosting@gmail.com>
2026-01-20 01:27:38 +0530

    Add TURN server support for WebRTC NAT traversal

--- src/components/WebcamStreamModal.jsx
+++ src/components/WebcamStreamModal.jsx
@@ -314,12 +314,28 @@ function WebcamStreamModal({ channel, isOpen, onClose, onUpdate }) {
       const startResponse = await api.post(`/webrtc/start/${channel.id}`);
       console.log('WebRTC session initialized:', startResponse.data);
 
-      // Create peer connection
+      // Create peer connection with TURN servers for NAT traversal
       const configuration = {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
-          { urls: 'stun:stun1.l.google.com:19302' }
-        ]
+          { urls: 'stun:stun1.l.google.com:19302' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
+        ],
+        iceCandidatePoolSize: 10
       };
 
       console.log('Creating RTCPeerConnection...');
